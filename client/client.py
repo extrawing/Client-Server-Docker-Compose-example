e244c77d5ac9dd060d038e1eefa9a1adfcb6ee7d
extrabit <extrawing@gmail.com>
2019-12-18 00:47:17 +0300

    added diag tools

--- client/client.py
+++ client/client.py
@@ -3,21 +3,20 @@
 # Import of python system library.
 # This library is used to download the 'index.html' from server.
 # You don't have to install anything special, this library is installed with Python.
-import urllib.request
+import urllib.request, time, sys
 
-# This variable contain the request on 'http://localhost:1234/'.
-# You must wondering what is 'http://localhost:1234'?
-# localhost: This means that the server is local.
-# 1234: Remember we define 1234 as the server port.
-fp = urllib.request.urlopen("http://server:1234/")
+def my_connect():
+    fp = urllib.request.urlopen("http://server:1234/", None, 5)
 
-# 'encodedContent' correspond to the server response encoded ('index.html').
-# 'decodedContent' correspond to the server response decoded (what we want to display).
-encodedContent = fp.read()
-decodedContent = encodedContent.decode("utf8")
+    encodedContent = fp.read()
+    decodedContent = encodedContent.decode("utf8")
+    print(decodedContent)
+    fp.close()
 
-# Display the server file: 'index.html'.
-print(decodedContent)
-
-# Close the server connection.
-fp.close()
+while True:
+    try:
+        my_connect()
+    except:
+        print("Something went wrong")
+    sys.stdout.flush()
+    time.sleep(20)
